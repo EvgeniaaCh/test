b6c971945a8913f2a7594a417881ab28b090efcb
Хайруллин Азат < >
2018-06-19 15:46:26 +0300

    Фикс

--- Exam/src/Sorter.java
+++ Exam/src/Sorter.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Sorter {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(123, 12, 12341234);
+        List<Integer> numbers = Arrays.asList(123, 121212);
         numbers.sort((o1, o2) -> {
             Long result1 = numberCounter(o1);
             Long result2 = numberCounter(o2);
@@ -16,10 +16,10 @@ public class Sorter {
     private static Long numberCounter(Integer someInteger) {
         String someString = someInteger.toString();
         long result = 0;
-        for (int i = 0; i < someString.length(); i++) {
-            char iChar = someString.charAt(i);
+        for (int i = 0; i < 10; i++) {
+            char iChar = (char) (i + '0');
             long charCount = someString.chars().filter(ch -> ch == iChar).count();
-            if (charCount == 1) {
+            if (charCount != 0) {
                 result++;
             }
         }
